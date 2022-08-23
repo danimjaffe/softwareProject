93bbfe94d587d98e7b54324675a4a8e62d7f911a
Ron Blachar <ronblachar@gmail.com>
2022-08-23 13:51:43 +0300

    Added matrix.c and matrix_stan.c

---
+++ matrix.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int rows;
+    int cols;
+    double ** data;
+} matrix;
+
+matrix * newMatrix(int rows, int cols) {
+    if (rows <= 0 || cols <= 0) return NULL;
+
+    // allocate a matrix structure
+    matrix * m = (matrix *) malloc(sizeof(matrix));
+
+    // set dimensions
+    m->rows = rows;
+    m->cols = cols;
+
+    // allocate a double array of length rows * cols
+    m->data = (double *) malloc(rows*cols*sizeof(double));
+    // set all data to 0
+    int i;
+    for (i = 0; i < rows*cols; i++)
+        m->data[i] = 0.0;
+
+    return m;
+}
