59d84d29162118bd0112d38a450f0ccc079cfc2e
Ron Blachar <ronblachar@gmail.com>
2022-08-29 17:25:28 +0300

    spkmeans.py v0 - not working but template should be fine

---
+++ kmeans.c
@@ -0,0 +1,210 @@
+#define PY_SSIZE_T_CLEAN
+#include "Python.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+static void convertPythonMatrixToCMatrix(PyObject *matrix,int numOfRows,int numOfColumns,double ***cMatrix);
+static PyObject* cMatrixToPythonObject(int k,int numOfColumns, double ***centroidArray);
+void update_centroids(double **centroids, double **data_points, int k, int cols, int rows, int max_iter, double eps);
+double **initialize_matrix(int rows, int cols);
+void free_matrix(double **matrix, int n_rows);
+int find_closest_centroid(double **centroids, double *vector, int k, int cols);
+double distance_between_vectors(double *vec1, double *vec2, int cols);
+int check_if_smaller_than_epsion(double **centroids, double **new_centroids, int k, int cols, double eps);
+
+static PyObject *fit(PyObject *self, PyObject * args)
+{
+    int k, max_iter, rows, dimension;
+    double **data_points, **centroids;
+    double eps;
+    PyObject *PycentArr;
+    PyObject *PypointArr;
+    PyObject *PyresultArr;
+
+    if(!PyArg_ParseTuple(args, "iidiiOO", &k, &max_iter,&eps, &dimension, &rows, &PycentArr, &PypointArr))
+    {
+        return NULL;
+    }
+
+    data_points=initialize_matrix(rows,dimension);
+    centroids=initialize_matrix(k,dimension);
+
+    convertPythonMatrixToCMatrix(PypointArr,rows,dimension, &data_points);
+    convertPythonMatrixToCMatrix(PycentArr,k,dimension, &centroids);
+
+    update_centroids(centroids, data_points, k, dimension, rows, max_iter, eps);
+
+    PyresultArr = cMatrixToPythonObject(k, dimension,&centroids);
+    
+    free_matrix(data_points, rows);
+    free_matrix(centroids, k);
+
+    return Py_BuildValue("O", PyresultArr);
+}
+
+static PyMethodDef capiMethods[] = {
+        {       "fit",
+                (PyCFunction) fit,
+                METH_VARARGS,
+                PyDoc_STR("K means algorithm!")},
+        {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "mykmeanssp",
+        NULL,
+        -1,
+        capiMethods
+};
+
+PyMODINIT_FUNC
+PyInit_mykmeanssp(void)
+{
+    PyObject *m;
+    m = PyModule_Create(&moduledef);
+    if (!m)
+    {
+        return NULL;
+    }
+    return m;
+}
+
+static void convertPythonMatrixToCMatrix(PyObject *matrix,int numOfRows,int numOfColumns,double ***cMatrix){
+    int i,j;
+    for(i=0; i<numOfRows;i++){
+        for(j = 0; j<numOfColumns;j++){
+            (*cMatrix)[i][j] = PyFloat_AsDouble(PyList_GetItem(matrix, j+(numOfColumns*i)));
+        }
+    }
+}
+
+static PyObject* cMatrixToPythonObject(int k,int numOfColumns, double ***centroidArray)
+{
+    int i,j;
+    PyObject *list = PyList_New(k*numOfColumns);
+    for(i=0; i<k; i++){
+        for(j=0;j<numOfColumns;j++){
+            PyList_SetItem(list, (i*numOfColumns)+j,PyFloat_FromDouble((*centroidArray)[i][j]));
+        }
+    }
+    return list;
+}
+
+void update_centroids(double **centroids, double **data_points, int k, int cols, int rows, int max_iter, double eps) {
+
+    int iteration_number = 0, epsilon_condition = 0;
+    int i, j, temp_index_of_closest_centroid;
+    double **new_centroids = initialize_matrix(k, cols);
+    int *counter_data_points_per_centroid = malloc(k * sizeof(int));
+    
+    assert (counter_data_points_per_centroid != NULL);
+    
+
+    while (iteration_number < max_iter && epsilon_condition == 0) {
+        iteration_number++;
+        for (i = 0; i < k; i++) {
+            counter_data_points_per_centroid[i] = 0;
+            for (j = 0; j < cols; j++) {
+                new_centroids[i][j] = 0.0;
+            }
+
+        }
+        for (i = 0; i < rows; i++) {
+            temp_index_of_closest_centroid = find_closest_centroid(centroids, data_points[i], k, cols);
+            counter_data_points_per_centroid[temp_index_of_closest_centroid]++;
+            for (j = 0; j < cols; j++) {
+                new_centroids[temp_index_of_closest_centroid][j] += data_points[i][j];
+            }
+        }
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < cols; j++) {
+                new_centroids[i][j] = new_centroids[i][j] / counter_data_points_per_centroid[i];
+            }
+        }
+
+        epsilon_condition = check_if_smaller_than_epsion(centroids, new_centroids, k, cols, eps);
+        if (epsilon_condition == 0) {
+            for (i = 0; i < k; i++) {
+                for (j = 0; j < cols; j++) {
+                    centroids[i][j] = new_centroids[i][j];
+                }
+
+            }
+
+        }
+
+    }
+    free(counter_data_points_per_centroid);
+    free_matrix(new_centroids, k);
+}
+
+double **initialize_matrix(int rows, int cols) {
+
+    double **a;
+    int i;
+    a = calloc(rows, sizeof(double *));
+    assert (centroids != NULL);
+    
+    for (i = 0; i < rows; i++){
+        a[i] = calloc(cols, sizeof(double));
+        assert (a[i] != NULL);
+    }
+        
+    return a;
+}
+
+void free_matrix(double **matrix, int n_rows) {
+    int i = 0;
+    for (; i < n_rows; i++)
+        free(matrix[i]);
+    free(matrix);
+}
+
+int find_closest_centroid(double **centroids, double *vector, int k, int cols) {
+    /*finds the row index within the centroids matrix of the closest centroid to a given vector*/
+    double tempdistance;
+    double min_distance = distance_between_vectors(centroids[0], vector, cols);
+    int min_distance_index = 0, i;
+    for (i = 1; i < k; i++) {
+        tempdistance = distance_between_vectors(centroids[i], vector, cols);
+        if (tempdistance < min_distance) {
+            min_distance = tempdistance;
+            min_distance_index = i;
+        }
+
+    }
+    return min_distance_index;
+
+}
+
+double distance_between_vectors(double *vec1, double *vec2, int cols) {
+    /*calculates the distance between 2 vectors (only calcluates the sum of all squared deltas, without taking the kth root)*/
+    double dis = 0.0;
+    int i;
+    for (i = 0; i < cols; i++) {
+        dis = dis + ((vec1[i] - vec2[i]) * (vec1[i] - vec2[i]));
+    }
+    return dis;
+
+}
+
+int check_if_smaller_than_epsion(double **centroids, double **new_centroids, int k, int cols, double eps) {
+    /*returns 1 if each of the centroids deltas are smaller than epsilon, 0 otherwise*/
+    int i, epsilon_dis_counter = 0;
+    double dis;
+    for (i = 0; i < k; i++) {
+        dis = distance_between_vectors(centroids[i], new_centroids[i], cols);
+        dis = sqrt(dis);
+        if (dis < eps) {
+            epsilon_dis_counter++;
+        }
+    }
+    if (epsilon_dis_counter == k) {
+        return 1;
+    }
+
+    return 0;
+}
--- spkmeans.py
+++ spkmeans.py
@@ -1,126 +1,129 @@
-# import mykmeanssp as kn
-# from math import sqrt
-# import argparse
-# import numpy as np
-# import pandas as pd
+import mykmeanssp as kn
+from math import sqrt
+import argparse
+import numpy as np
+import pandas as pd
 
-# np.random.seed(0)
-
-
-# def get_data(filename):
-#     df = pd.read_csv(filename, index_col=0, header=None)
-#     return df
+np.random.seed(0)
 
 
-# def output_results(results, centroids_idx, n_dimensions):
-#     print(','.join(centroids_idx))
-
-#     new_line = "\n"
-#     res_str = [f'{res:.4f}{new_line if (i + 1) % n_dimensions == 0 else ","}'
-#                for i, res in enumerate(results)]
-#     res_str = ''.join(res_str)
-#     print(res_str)
+def get_data(filename):
+    df = pd.read_csv(filename, index_col=0, header=None)
+    return df
 
 
-# def flatten_data(matrix):
-#     data = []
-#     for x in matrix:
-#         for y in x:
-#             data.append(y)
-#     return data
+def output_results(results, centroids_idx, n_dimensions):
+    print(','.join(centroids_idx))
+    new_line = "\n"
+    res_str = [f'{res:.4f}{new_line if (i + 1) % n_dimensions == 0 else ","}'
+               for i, res in enumerate(results)]
+    res_str = ''.join(res_str)
+    print(res_str)
 
 
-# class KmeansPP:
-#     def __init__(self, data, n_dimensions, k, max_iter=200):
-#         self.data = data
-#         self.data_arr = data.to_numpy()
-#         self.n_dimensions = n_dimensions
-#         self.k = k
-#         self.max_iter = max_iter
-#         rand_idx = np.random.choice(self.data.index)
-#         self.centroids_idx = [str(int(rand_idx))]
-#         self.centroids = [self.data.loc[rand_idx].to_numpy()]
-
-#     def initialize_centroids(self):
-#         for i in range(1, self.k):
-#             self.calc_centroid()
-#         return self.centroids
-
-#     def calc_centroid(self):
-#         prob_func = self.calc_prob()
-#         rand_idx = np.random.choice(self.data.index, p=prob_func)
-#         self.centroids_idx.append(str(int(rand_idx)))
-#         self.centroids.append(self.data.loc[rand_idx])
-
-#     def calc_prob(self):
-#         dist_sum = 0
-#         min_dist_arr = np.empty(shape=len(self.data))
-#         for i, point in enumerate(self.data_arr):
-#             min_dist = euclidean_distance(point, self.centroids[0])
-#             for centroid in self.centroids:
-#                 curr_dist = euclidean_distance(point, centroid)
-#                 if curr_dist < min_dist:
-#                     min_dist = curr_dist
-#             min_dist_arr[i] = min_dist
-#             dist_sum += min_dist
-#         return min_dist_arr / dist_sum
+def flatten_data(matrix):
+    data = []
+    for x in matrix:
+        for y in x:
+            data.append(y)
+    return data
 
 
-# ####################
-# # Helper Functions #
-# ####################
+class KmeansPP:
+    def __init__(self, data, k, max_iter=200):
+        self.data = data
+        self.data_arr = data.to_numpy()
+        self.k = k
+        self.max_iter = max_iter
+        rand_idx = np.random.choice(self.data.index)
+        self.centroids_idx = [str(int(rand_idx))]
+        self.centroids = [self.data.loc[rand_idx].to_numpy()]
 
-# def euclidean_norm(u, v):
-#     squared_dist = euclidean_distance(u, v)
-#     norm = sqrt(squared_dist)
-#     return norm
+    def initialize_centroids(self):
+        for i in range(1, self.k):
+            self.calc_centroid()
+        return self.centroids
 
+    def calc_centroid(self):
+        prob_func = self.calc_prob()
+        rand_idx = np.random.choice(self.data.index, p=prob_func)
+        self.centroids_idx.append(str(int(rand_idx)))
+        self.centroids.append(self.data.loc[rand_idx])
 
-# def euclidean_distance(u, v):
-#     assert len(v) == len(u)
-#     euclidean_dist = 0
-#     for dim in range(len(u)):
-#         euclidean_dist += pow(u[dim] - v[dim], 2)
-#     return euclidean_dist
-
-
-# def validate_input(k, max_iter, n_rows, eps):
-#     assert (n_rows > k > 1)
-#     assert (max_iter > 0)
-#     assert (eps >= 0)
-
-
-# class ArgumentParser(argparse.ArgumentParser):
-#     def error(self, message):
-#         self.exit(2)
-
-
-# def main():
-#     try:
-#         parser = ArgumentParser()
-#         parser.add_argument("k", type=int)
-#         parser.add_argument("goal", type=str)
-#         parser.add_argument("file_name", type=str)
-#         args = parser.parse_args()
-#         k = args.k
-#         goal = args.goal
-#         file_name = args.file_name
-#         data = get_data(file_name)
-#         validate_input(k, goal, file_name)
-#     except:
-#         raise SystemExit('Invalid Input!')
-
-#     try:
-#         kmeans_pp = KmeansPP(data, n_dimensions, k, max_iter=300)
-#         centroids_idx = kmeans_pp.centroids_idx
-#         data = flatten_data(kmeans_pp.data_arr)
-#         centroids = kmeans_pp.initialize_centroids()
-#         centroids = flatten_data(centroids)
-#         result = kn.fit(k, max_iter, eps, n_dimensions, number_of_rows, centroids, data)
-#         output_results(result, centroids_idx, n_dimensions)
-#     except:
-#         raise SystemExit('An Error Has Occurred')
+    def calc_prob(self):
+        dist_sum = 0
+        min_dist_arr = np.empty(shape=len(self.data))
+        for i, point in enumerate(self.data_arr):
+            min_dist = euclidean_distance(point, self.centroids[0])
+            for centroid in self.centroids:
+                curr_dist = euclidean_distance(point, centroid)
+                if curr_dist < min_dist:
+                    min_dist = curr_dist
+            min_dist_arr[i] = min_dist
+            dist_sum += min_dist
+        return min_dist_arr / dist_sum
 
 
-# if __name__ == '__main__':
-#     main()
+####################
+# Helper Functions #
+####################
+
+def euclidean_norm(u, v):
+    squared_dist = euclidean_distance(u, v)
+    norm = sqrt(squared_dist)
+    return norm
+
+
+def euclidean_distance(u, v):
+    assert len(v) == len(u)
+    euclidean_dist = 0
+    for dim in range(len(u)):
+        euclidean_dist += pow(u[dim] - v[dim], 2)
+    return euclidean_dist
+
+
+def validate_input(k, max_iter, n_rows, filename):
+    assert (n_rows > k > 1)
+    assert (max_iter > 0)
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        self.exit(2)
+
+
+def main():
+    try:
+        parser = ArgumentParser()
+        parser.add_argument("k", type=int)
+        parser.add_argument("goal", type=str)
+        parser.add_argument("file_name", type=str)
+        args = parser.parse_args()
+        k = args.k
+        goal = args.goal
+        file_name = args.file_name
+        data = get_data(file_name)
+        n_dimensions = len(data.columns)  # TODO - make sure necessary!
+        number_of_rows = len(data)  # TODO - make sure necessary!
+        validate_input(k, goal, number_of_rows, file_name)
+    except:
+        raise SystemExit('Invalid Input!')
+    try:
+        # TODO:
+        #  1. runGoalPy(goal, data, k) and run KMeansPP on runGoalPy's results!
+        #  2. make sure we pass the right dimensions from this point forward!
+        #  3. make sure all constants are correct
+        max_iter = 300
+        kmeans_pp = KmeansPP(data, k, max_iter)  # TODO - make sure max_iter is correct!
+        centroids_idx = kmeans_pp.centroids_idx
+        data = flatten_data(kmeans_pp.data_arr)
+        centroids = kmeans_pp.initialize_centroids()
+        centroids = flatten_data(centroids)
+        result = kn.fit(k, max_iter, 0.00001, n_dimensions, number_of_rows, centroids, data)  # TODO - make sure max_iter is correct!
+        output_results(result, centroids_idx, n_dimensions)
+    except:
+        raise SystemExit('An Error Has Occurred')
+
+
+if __name__ == '__main__':
+    main()
